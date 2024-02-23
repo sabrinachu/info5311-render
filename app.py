f0b03411e678a20e0409e605f3ad2b35d0f3fb23
sc2323 <sc2323@cornell.edu>
2024-02-23 01:14:54 -0500

    second homework

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import *
+
+from flask import * 
 from whitenoise import WhiteNoise
 
-app = Flask(__name__)
-app.wsgi_app = WhiteNoise(app.wsgi_app, 
-                          root='static/', 
-                          prefix='static/', 
-                          index_file='index.htm', 
-                          autorefresh=True)
+app = Flask(__name__) 
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/', prefix='static/', index_file='index.htm', autorefresh=True)
+
 
 @app.route('/', methods=['GET'])
-def hello():
-    return make_response("Hello, world!!!!!!!!!!!!!!!!")
-    
+def hello(): 
+    return render_template('index.htm')
+
 if __name__ == "__main__":
     app.run(threaded=True, port=5000)
